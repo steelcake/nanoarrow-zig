25293ec75a4fd93e75e4132019a18b602991c7d2
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-11-23 22:43:15 +0600

    export better validate api

--- validate/root.zig
+++ validate/root.zig
@@ -1,5 +1,16 @@
-pub extern fn nanoarrowzig_validate(
+extern fn nanoarrowzig_validate(
     input_array: *const anyopaque,
     input_schema: *const anyopaque,
     validate_fail: *const fn () callconv(.c) void,
 ) void;
+
+pub fn validate(
+    input_array: *const anyopaque,
+    input_schema: *const anyopaque,
+) void {
+    nanoarrowzig_validate(input_array, input_schema, validate_fail);
+}
+
+fn validate_fail() callconv(.c) void {
+    unreachable;
+}
