16517c46d01aafbfcc4d7917fad21fb3484a6bfa
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-11-23 20:31:40 +0600

    pass callback for triggering validation failure

--- validate/root.zig
+++ validate/root.zig
@@ -1,1 +1,5 @@
-pub extern fn nanoarrowzig_validate(input_array: *const anyopaque, input_schema: *const anyopaque) void;
+pub extern fn nanoarrowzig_validate(
+    input_array: *const anyopaque,
+    input_schema: *const anyopaque,
+    validate_fail: *const fn () callconv(.c) void,
+) void;
--- validate_sys/validate.c
+++ validate_sys/validate.c
@@ -4,7 +4,8 @@
 
 void nanoarrowzig_validate(
   struct ArrowArray* const input_array,
-  struct ArrowSchema* const input_schema
+  struct ArrowSchema* const input_schema,
+  void (*validate_fail)(void)
 ) {
   if (!input_array || !input_schema) {
     fprintf(stderr, "Error: NULL pointer\n");
@@ -20,18 +21,18 @@ void nanoarrowzig_validate(
   result = ArrowArrayViewInitFromSchema(&view, input_schema, &error);
   if (result != NANOARROW_OK) {
     fprintf(stderr, "ArrowArrayViewInitFromSchema failed: %s\n", ArrowErrorMessage(&error));
-    exit(1);
+    validate_fail();
   }
 
   result = ArrowArrayViewSetArray(&view, input_array, &error);
   if (result != NANOARROW_OK) {
     fprintf(stderr, "ArrowArrayViewSetArray failed: %s\n", ArrowErrorMessage(&error));
-    exit(1);
+    validate_fail();
   }
 
   result = ArrowArrayViewValidate(&view, NANOARROW_VALIDATION_LEVEL_FULL, &error);
   if (result != NANOARROW_OK) {
     fprintf(stderr, "ArrowArrayViewValidate failed: %s\n", ArrowErrorMessage(&error));
-    exit(1);
+    validate_fail();
   }
 }
